11b73debc4bdec2283fe91f02187230042367c45
NecroBread <mariovyordanov@gmail.com>
2022-09-27 08:44:26 +0300

    add: animation frame

--- index.js
+++ index.js
@@ -16,17 +16,29 @@ class Player {
             y: 0,
         };
 
-        // this.image = 
+        const image = new Image();
+        image.src = './assets/spaceship.png'
+
+        this.image = image;
         this.width = 100;
         this.height = 100;
     }
 
     draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // c.fillStyle = 'red';
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.drawImage(this.image, this.position.x, this.position.y);
     }
 
 }
 
 const player = new Player();
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+}
+
+animate();
