fd2cb39a2ed7b47a83ccef2c7f92086c9956f3f7
NecroBread <mariovyordanov@gmail.com>
2022-09-27 09:56:35 +0300

    create invader class

--- index.js
+++ index.js
@@ -81,8 +81,45 @@ class Projectile {
     }
 }
 
+class Invader {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+
+        const image = new Image();
+        image.src = './assets/invader.png'
+        image.onload = () => {
+            const scale = 1;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height / 2,
+            };
+        }
+    }
+
+    draw() {
+        // c.fillStyle = 'red';
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
+    }
+
+}
 const player = new Player();
 const projectiles = [];
+const invader = new Invader();
 
 const keys = {
     a: {
@@ -99,9 +136,13 @@ const keys = {
 // game loop
 function animate() {
     requestAnimationFrame(animate);
+
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
+
+    invader.update()
     player.update()
+
     projectiles.forEach((projectile, i) => {
         if (projectile.position.y + projectile.radius <= 0) {
             setTimeout(() => {
