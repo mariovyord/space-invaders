d1528ac62fc7b5b9dcc078d46a7676526e0df3ba
NecroBread <mariovyordanov@gmail.com>
2022-09-27 08:38:16 +0300

    create: player class

---
+++ index.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('canvas');
+const c = canvas?.getContext('2d');
+
+// Set canvas size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 200,
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+
+        // this.image = 
+        this.width = 100;
+        this.height = 100;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+}
+
+const player = new Player();
+player.draw()
