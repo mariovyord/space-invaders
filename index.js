cb4c31026e34bc40ef780a72e31e0ccef06e6d3e
NecroBread <mariovyordanov@gmail.com>
2022-09-27 09:12:47 +0300

    add player movement

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const canvas = document.querySelector('canvas');
 const c = canvas?.getContext('2d');
 
 // Set canvas size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth - 5;
+canvas.height = window.innerHeight - 5;
 
 class Player {
     constructor() {
@@ -22,7 +22,7 @@ class Player {
 
             this.position = {
                 x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - image.height - 20,
+                y: canvas.height - (image.height * scale) - 30,
             };
         }
     }
@@ -34,16 +34,72 @@ class Player {
             c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
         }
     }
+    update() {
+        if (this.image) {
+            this.draw();
+            this.position.x += this.velocity.x;
+        }
+    }
 
 }
 
 const player = new Player();
+const keys = {
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+    space: {
+        pressed: false,
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
-    player.draw()
+    player.update()
+
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -5;
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 5;
+    } else {
+        player.velocity.x = 0;
+    }
 }
 
 animate();
+
+// move player
+window.addEventListener('keydown', (e) => {
+    const key = e.key;
+    switch (key) {
+        case 'a':
+            keys.a.pressed = true;
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            break;
+        case ' ':
+            keys.space.pressed = true;
+            break;
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    const key = e.key;
+    switch (key) {
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case ' ':
+            keys.space.pressed = false;
+            break;
+    }
+})
